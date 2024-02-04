703f5eea4565e96cdd6f1b605a4774496dc993b4
Saltykub <nathan.juir@gmail.com>
2024-02-04 20:03:45 +0800

    form fix kub

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,26 +7,25 @@ Typography} from "@material-tailwind/react"
 import TextInput from "./TextInput"
 import SelectInput from "./SelectInput"
 const Form = (props) => {
+    const {input, setInput} = props
     const handleSubmit = e => {
         e.preventDefault() 
-    }
-    const handleChange = e => {
-        console.log(e.target.value)
     }
     return (
     <Card color="transparent" shadow={false} className="items-center justify-center">
       <Typography variant="h4" color="blue-gray" className="mt-11"> 
-        Input
+        Taste and Trek
       </Typography>
       <Typography color="gray" className="mt-1 font-normal">
-        Nice to meet you! Enter your details to register.
+        Fill your information 
       </Typography>
       <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleSubmit}>
         <div className="mb-1 flex flex-col gap-6">
-          <TextInput name="Preference" holder="Your preference" handleChange={handleChange}/>
-          <SelectInput options={["a","b","c","d","e"]} label="wow" handleChange={handleChange}/>
+          <TextInput name="Preference" holder="Your preference" input={input} setInput={setInput} index="preference"/>
+          <TextInput name="Recent Travel" holder="Your recent travel" input={input} setInput={setInput} index="recent_travel" />
+          <SelectInput options={["male","female","other"]} label="select gender"/>
         </div>
-        <Button className="mt-6" fullWidth>
+        <Button  type="submit"  className="mt-6" fullWidth>
           submit
         </Button>
         
