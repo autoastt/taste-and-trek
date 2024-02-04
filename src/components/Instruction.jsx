09165ef096a449c25afb6f8580061e527810ee2f
Fair <kulpatch.chananam@gmail.com>
2024-02-04 22:46:50 +0800

    add instruction: Find Restaurant or Place

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -3,16 +3,15 @@ import { Typography } from '@material-tailwind/react'
 const Instruction = () => {
   return (
     <div>
-      <Typography className = "text-indigo-500 text-5xl font-bold">
+      <Typography className = "text-indigo-500 text-5xl font-bold text-center">
         Plan your day!
       </Typography>
-      <Typography className = "text-lg" color = "black">
-        Find <span className ="font-bold">Restaurant</span> or <span className ="font-bold">Place</span> to go with only 3 steps:
+      <Typography className = "text-xl text-center" color = "black">
+        Find <span className = "font-bold">Restaurant</span> or <span className ="font-bold">Place</span> to go within 60 seconds
       </Typography>
-      <Typography variant = "Lead" color = "black">
-        1st - type what you want<br></br>
-        2nd - <br></br>
-        3rd - d
+      <Typography className = "text-center grid grid-cols-2" color = "black">
+        <span className = "text-center text-base">Tell us what you're looking for</span> 
+        <span className = "text-center text-base">Edit</span>
       </Typography>
     </div>
   )
