bd872dbd1f11fd7a674e4a778e082a73faa4c214
Fair <kulpatch.chananam@gmail.com>
2024-02-04 23:17:16 +0800

    add Instruction: Fill in yours

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -1,20 +1,25 @@
-import { Typography } from '@material-tailwind/react'
+import { Typography } from "@material-tailwind/react";
 
 const Instruction = () => {
   return (
     <div>
-      <Typography className = "text-indigo-500 text-5xl font-bold text-center">
-        Plan your day!
+      <Typography className="text-indigo-500 text-5xl font-bold text-center">
+        Plan your day!<br></br>
       </Typography>
-      <Typography className = "text-xl text-center" color = "black">
-        Find <span className = "font-bold">Restaurant</span> or <span className ="font-bold">Place</span> to go within 60 seconds
+      <Typography className="text-xl text-center" color="black">
+        Find <span className="font-bold">Restaurant</span> or{" "}
+        <span className="font-bold">Place</span> to go within 60 seconds
       </Typography>
-      <Typography className = "text-center grid grid-cols-2" color = "black">
-        <span className = "text-center text-base">Tell us what you're looking for</span> 
-        <span className = "text-center text-base">Edit</span>
+      <Typography className="text-center" color="black">
+        <span className="text-center text-lg">
+          <span className = "font-bold text-indigo-500">Tell us</span> what you're looking for by<br></br>
+        </span>
+        <span>
+          Fill in your Information, Budget, and Preference<br></br>
+        </span>
       </Typography>
     </div>
-  )
-}
+  );
+};
 
-export default Instruction
+export default Instruction;
