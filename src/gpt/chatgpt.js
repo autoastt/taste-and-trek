cafa85aee8bf2cde4534b0c50d84cb8e6e46adcf
Petch Pisutsin <53936910+PatrickPetch@users.noreply.github.com>
2024-02-04 15:54:55 +0800

    update gpt text generation code

---
+++ src/gpt/chatgpt.js
@@ -0,0 +1,46 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+async function chatgpt(data) {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `Please recommmend a plan on food and travel for the entire day given the following information.
+
+        The person is a ${data.status} who is travelling with ${data.relationship} on a ${data.purpose} with a ${data.budget} budget.
+        Preferences: ${data.preference == "" ? "None" : data.preference}
+        Avoid: ${data.recent_travel == "" ? "None" : data.recent_travel}.
+
+        List down two activities and three meals (b/l/d) from morning to evening, considering the travel time between meals and activities to be not too long,
+        with the following information, all separated by semicolon on the same line
+          (1) name
+          (2) specific location (postal address) + shopping mall / attraction name if any 
+          (3) short attractive description of each place
+        List all the activities and meals without number label or bold texts, but label if it is an activity or which meal it is.`,
+      },
+    ],
+    model: "gpt-4-0125-preview",
+  });
+
+  return completion["choices"][0]["message"]["content"];
+}
+
+var data = {
+  status: "resident",
+  relationship: "family",
+  purpose: "family trip",
+  budget: "high",
+  preference: "local Singaporean food and indoor",
+  recent_travel: "Garden by the Bay",
+};
+
+chatgpt(data)
+  .then((result) => console.log(result))
+  .catch((error) => console.error("Test Error:", error));
+
+export default chatgpt;
