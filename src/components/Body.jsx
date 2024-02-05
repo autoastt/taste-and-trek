2619ededf5b81980d3fb857443d7c6eaec0efbf9
autoastt <peeranat.kjp@gmail.com>
2024-02-05 13:40:48 +0800

    loading for pic and text

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import Form from "./Form"
 import data from "../assets/data"
-import { Card, Typography } from "@material-tailwind/react"
+import { Card, Spinner, Typography } from "@material-tailwind/react"
 import Instruction from "./Instruction"
 import outputData from "../assets/outputData"
 const Body = () => {
@@ -17,11 +17,11 @@ const Body = () => {
         <div className="mb-4 lg:hidden"><Form input={input} setInput={setInput} output={output} setOutput={setOutput} loading={loading} setLoading = {setLoading}/></div>
         <Card className="items-center pb-8 px-4 mb-4">
           <Typography variant="h2" color="black" className="py-4">Your Poster</Typography>
-          <img src={output['url']} className="h-auto w-4/5 md:max-w-lg object-cover overflow-hidden rounded-[2rem]"/>
+          {loading ? <Spinner className="size-12"/> : <img src={output['url']} className="h-auto w-4/5 md:max-w-lg object-cover overflow-hidden rounded-[2rem]"/>}
         </Card>
         <Card className="pb-8 px-6">
           <Typography variant="h2" color="black" className="text-center py-4">Your Plan</Typography>
-          {(output.text).map(out => 
+          {loading ? <Spinner className="self-center size-12"/>: (output.text).map(out => 
             <div key={out} className="pb-3">
               <Typography variant="h5"> {out.split(';')[0]} </Typography>
               <Typography variant="h6">Location: {out.split(';')[1]} </Typography>
