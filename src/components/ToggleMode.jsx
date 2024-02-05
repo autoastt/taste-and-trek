c467f89433e7a5ae4775604bf4371fd60493ce19
autoastt <peeranat.kjp@gmail.com>
2024-02-05 19:51:40 +0800

    fix dark

--- src/components/ToggleMode.jsx
+++ src/components/ToggleMode.jsx
@@ -1,35 +1,33 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import { Button, IconButton } from "@material-tailwind/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ToggleMode = () => {
-  const [theme, setTheme] = useState(localStorage.theme)
-
-  function handleClick() {
-    if (localStorage.theme === "dark" || !("theme" in localStorage)) {
-      //add class=dark in html element
-      document.documentElement.classList.add("dark");
+  const [theme, setTheme] = useState("light")
+  // console.log(theme)
+  
+  const handleClick = () => {
+    if (localStorage.getItem("theme") === "dark" || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add("dark")
     } else {
-      //remove class=dark in html element
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove("dark")
     }
   
     if (localStorage.theme === "dark") {
       localStorage.theme = "light";
+      setTheme("dark")
     } else {
       localStorage.theme = "dark";
+      setTheme("light")
+
     }
-    setTheme(localStorage.theme)
   }
 
   return <IconButton variant="text" onClick={handleClick} className="">
-    {theme === "dark" ? 
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-    </svg> 
+    {theme === "light" ? 
+    <SunIcon className="size-5" strokeWidth={2} />
     :
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-white" fill="white">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-    </svg>
+    <MoonIcon className="size-4 fill-white text-white" strokeWidth={2} />
     }
   </IconButton>
 }
