7eef391fab5998f0553498c4b5781a16a266a431
Saltykub <nathan.juir@gmail.com>
2024-02-05 21:04:45 +0800

    fix

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,7 +3,7 @@ const Map = (props) => {
     // const res = search("Pop mart Jurong point")
     const {mapId} = props
     const googleMapAPI = process.env.GOOGLEMAP_API_KEY
-    const src = `https://www.google.com/maps/embed/v1/directions?key=${googleMapAPI}&origin=place_id:${mapId[0]}&destination=place_id:${mapId[4]}&waypoints=place_id:${mapId[2]}|place_id:${mapId[3]}|place_id:${mapId[1]}`
+    const src = `https://www.google.com/maps/embed/v1/directions?key=${googleMapAPI}&origin=place_id:${mapId[0]}&destination=place_id:${mapId[4]}&waypoints=place_id:${mapId[1]}|place_id:${mapId[2]}|place_id:${mapId[3]}`
     return (
         <div>
         <iframe
